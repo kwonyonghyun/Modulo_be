a62ded370af1fd316b2538b063d6fefed5d0d138
권용현 <gwon-yonghyeon@gwon-yonghyeon-ui-MacBookAir.local>
2024-12-31 14:53:42 +0900

    [Chore] 자기소개서 응답에 id 추가

--- src/main/java/com/example/Modulo/dto/response/SelfIntroductionResponse.java
+++ src/main/java/com/example/Modulo/dto/response/SelfIntroductionResponse.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @Builder
 public class SelfIntroductionResponse {
 
+    private Long id;
     private String title;
     private String content;
 
     public static SelfIntroductionResponse from(SelfIntroduction selfIntroduction) {
         return SelfIntroductionResponse.builder()
+                .id(selfIntroduction.getId())
                 .title(selfIntroduction.getTitle())
                 .content(selfIntroduction.getContent())
                 .build();
